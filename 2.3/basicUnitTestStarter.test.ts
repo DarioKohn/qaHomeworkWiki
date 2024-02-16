fa0b7da6da91e2d5503d0e7b3528722bfa4b9636
Dario Kohn <153893307+DarioKohn@users.noreply.github.com>
2024-02-16 03:47:10 -0500

    Add files via upload

--- 2.3/basicUnitTestStarter.test.ts
+++ 2.3/basicUnitTestStarter.test.ts
@@ -1,48 +1,63 @@
 describe("Testing the calculator", () => {
-    test("Example test", () => {
-      expect(calculator.add(2, 50)).toBe(52);
+  test("Example test", () => {
+    expect(calculator.add(2, 50)).toBe(52);
+  });
+  test("Addition works", () => {
+    expect(calculator.add(10, 50)).toBe(60);
+  });
+  test("Subtraction works", () => {
+    expect(calculator.subtract(20, 5)).toBe(15);
+  });
+  test("Multiplication works", () => {
+    expect(calculator.multiply(10, 5)).toBe(50);
+  });
+  test("Division works", () => {
+    expect(calculator.divide(20, 4)).toBe(5);
+  });
+  test ("Test of two variables equal to each other", () => {
+    let x = "equal"
+    let y = "equal"
+    expect(x == y).toBeTruthy();
+  });
+  test ("Test that one number is greater than another", () => {
+    let x = 50
+    let y = 10
+  expect(x > y).toBeTruthy();
+  });
+  test ("Test that one number is less than another", () => {
+    let x = 20
+    let y = 40
+  expect(x < y).toBeTruthy();
+  });
+  describe("Stretch Goals", () => {
+    test("(2+2)/4 == 1", () => {
+      expect(calculator.divide(calculator.add(2, 2), 4)).toBe(1);
     });
-    test("Addition works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Subtraction works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Multiplication works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Division works", () => {
-      expect(true).toBeTruthy();
-    });
-    describe("Stretch Goals", () => {
-      test("(2+2)/4 == 1", () => {
-        expect(calculator.divide(calculator.add(2, 2), 4)).toBe(1);
-      });
-      test("(28/4)*(6/2)*(30-27+(3-4)) == 42", () => {
-        expect(true).toBeTruthy();
-      });
+    test("(28/4)*(6/2)*(30-27+(3-4)) == 42", () => {
+      expect(calculator.multiply((calculator.multiply(calculator.divide(28, 4), calculator.divide(6, 2))), calculator.add(calculator.subtract(30, 27), calculator.subtract(3, 4)))).toBe(42);
     });
   });
-  
-  const calculator = {
-    /** Takes two numbers and adds them together.
-     * @example
-     * calculator.add(2, 3) == 5
-     */
-    add: (a: number, b: number): number => a + b,
-    /** Takes two numbers and subtracts the second from the first.
-     * @example
-     * calculator.subtract(2, 3) == -1
-     */
-    subtract: (a: number, b: number): number => a - b,
-    /** Takes two numbers and multiplies them together.
-     * @example
-     * calculator.multiply(2, 3) == 6
-     */
-    multiply: (a: number, b: number): number => a * b,
-    /** Takes two numbers and divides the first by the second.
-     * @example
-     * calculator.divide(2, 3) == 0.5
-     */
-    divide: (a: number, b: number): number => a / b,
-  };
+});
+
+const calculator = {
+  /** Takes two numbers and adds them together.
+   * @example
+   * calculator.add(2, 3) == 5
+   */
+  add: (a: number, b: number): number => a + b,
+  /** Takes two numbers and subtracts the second from the first.
+   * @example
+   * calculator.subtract(2, 3) == -1
+   */
+  subtract: (a: number, b: number): number => a - b,
+  /** Takes two numbers and multiplies them together.
+   * @example
+   * calculator.multiply(2, 3) == 6
+   */
+  multiply: (a: number, b: number): number => a * b,
+  /** Takes two numbers and divides the first by the second.
+   * @example
+   * calculator.divide(2, 3) == 0.5
+   */
+  divide: (a: number, b: number): number => a / b,
+};
