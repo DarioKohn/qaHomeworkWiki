4551295cc627711bcdc891af2cec89eb5e6ed275
admin <admin@admins-MacBook-Pro-2.local>
2024-02-28 00:39:37 -0500

    2.7 homework

--- 2.7/2.7Test.test.ts
+++ 2.7/2.7Test.test.ts
@@ -1,23 +1,43 @@
 import { pageObject } from "./2.7PageObject";
-const page = new pageObject(); 
+const page = new pageObject();
 
 class Intern {
    //create a class for interns that includes the name phone and title for the intern.
+   name: string;
+   phone: number;
+   title: string;
+
+   constructor(name: string, phone: number, title: string) {
+      this.name =name;
+      this.phone = phone;
+      this.title = title;
+  };
 }; 
 
 let newInterns: Array<Intern> = [
-   //create an array of Interns using your class 
+   //create an array of Interns using your class
+   new Intern("Dario Kohn", 1123456789, "Absolute beginner"),
+   new Intern("Caroline Kohn", 9876543212, "Poet"),
+   new Intern("Nadezda Kohn", 1123456789, "Misfortunate 1"),
+   new Intern("Bini Kohn", 9876543212, "Misfortunate 2")
 ];
 
 let addInterns = async (newInterns) => {
    //create a function to add the interns from your array
+   await page.click(page.addEm);
+   await page.click(page.newEm);
+   await page.setInput(page.namInp, newInterns.name);
+   await page.setInput(page.phoInp, newInterns.phone);
+   await page.setInput(page.titleInp, newInterns.title);
+   await page.click(page.saveBtn);
 };
 
 test('can add the bake off crew',async () => {
-    await two.navigate(); 
-    await two.getElement(two.addEm); 
+    await page.navigate();
+    await page.getElement(page.addEm);
     for(let i= 0; i < newInterns.length; i ++){
-        await addInterns(newInterns[i]); 
-    }; 
-    await two.driver.quit(); 
+        await addInterns(newInterns[i]);
+    };
+    await page.driver.sleep(2000);
+    await page.driver.quit();
 })
